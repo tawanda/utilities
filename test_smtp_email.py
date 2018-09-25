5eb874e480b3c32ff67cd80ddd6417fb0b211354
Tawanda Minya <tminya@gmail.com>
2018-09-25 08:47:49 +0200

    Create test_smtp_email.py

---
+++ test_smtp_email.py
@@ -0,0 +1,33 @@
+#last tested 2018
+
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.server.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print('successfully sent the mail')
+    except Exception as er:
+        print("failed to send mail -" + str(er))
+
+
+user = ''
+pwd =  ''
+recipient = ''
+
+subject = 'test'
+body = 'testing mail'
+
+send_email(user, pwd, recipient, subject, body)
