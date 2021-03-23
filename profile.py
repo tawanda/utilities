8dcacf98ef7dbe9f6277eaad52d072ea026b5189
Tawanda Minya <tminya@gmail.com>
2021-03-23 10:54:41 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,12 +1,13 @@
 """
 USAGE
+----------------------------
 
     @profileit
     def function_you_want_to_profile(...)
         ...
 
 HOWTO
-
+----------------------------
 Add this decorator to any function you want to profile, it generates a .profile file in the root dir
 e.g get()__2021_03_23__06_20_38.profile
 You can open this file in pycharm with
@@ -15,6 +16,32 @@ Tools->Open cProfile Snapshot  # Very last option at bottom
 
 or you cna use something like https://jiffyclub.github.io/snakeviz/
 
+UNDERSTANDING THE RESULTS
+----------------------------
+
+https://stackoverflow.com/questions/54118630/understanding-pycharms-profilers-results-vs-cprofile-results-and-how-to-get-m#:~:text=Own%20time%20%2D%20Own%20execution%20time,this%20call%20in%20the%20parentheses.
+
+
+SAMPLE CODE TO TEST THIS ON
+----------------------------
+
+```
+import time
+
+
+def takes_2_seconds():
+    time.sleep(2)
+
+
+def takes_1_second():
+    [a+b for a in range(10000) for b in range(10000)]
+    time.sleep(1)
+    takes_2_seconds()
+
+
+if __name__ == '__main__':
+    takes_1_second()
+```
 """
 
 import cProfile
